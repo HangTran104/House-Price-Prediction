d60db031dcdd7f098d55b9f8118af835e9590c7b
HangTran104 <90120107+HangTran104@users.noreply.github.com>
2022-10-26 15:21:08 +0700

    Update Deployments.py

--- Deployments.py
+++ Deployments.py
@@ -37,20 +37,18 @@ def main():
   Dien_tich_su_dung = st.text_input('The total usage area of the house')
   toilet = st.text_input('Number of toilet you want the house has')
   hem = st.text_input('The width of alley around the house')
+  so_lau = st.text_input('The number of floors you want')
   chieudai = st.text_input('the length of the house')
   so_phong = st.text_input('Numbers of rooms')
   nhabe = st.text_input('Your house is needed to located in Nha Be District or not? 0 for No and 1 for Yes')
   rong = st.text_input('The width of the house')
-  quan10 = st.text_input('Your house is needed to located in Quan 10 or not?')
-  
-  df_input = pd.DataFrame(['Dien_tich', 'Dien_tich_su_dung', 'Nha_ve_sinh', 'Hem_rong', 'So_lau',
-       'Chieu_dai', 'So_phong', 'Quan_Huyện Nhà Bè', 'Rong', 'Duong_mat_tien',
-       'Quan_Quận 10']
+  duongmattien = st.text_input('Do you want your house is located next to the big streets? 0 for No and 1 for Yes?')
+  quan10 = st.text_input('Your house is needed to located in Quan 10 or not?0 for No and 1 for Yest?')
 
   # Code for prediction
-  houseprice = ''
+  houseprice = 0
   if st.button('House Price Prediction'):
-    houseprice = HousePrice_Prediction([Dientich, Dien_tich_su_dung, toilet, hem, chieudai, so_phong, nhabe, rong, quan10])
+    houseprice = HousePrice_Prediction([Dientich, Dien_tich_su_dung, toilet, hem, so_lau, chieudai, so_phong, nhabe, rong, duongmattien, quan10])
 
     st.success(houseprice)
 
